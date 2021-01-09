1e01958e0df9f5b8d400e82bb849fe64ec92a159
Kevin Mok <kmok@redhat.com>
2021-01-08 19:49:22 -0500

    [KOGITO-1031] Run BDD tests from PR

--- jenkins/tests/src/test/vars/JenkinsfileNightly.groovy
+++ jenkins/tests/src/test/vars/JenkinsfileNightly.groovy
@@ -1,11 +1,27 @@
 import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
 
 class JenkinsfileNightly extends JenkinsPipelineSpecification {
-	
-	def '[Jenkinsfile.nightly] test load script' () {
+	def Jenkinsfile = null
+
+    def setup() {
+        Jenkinsfile = loadPipelineScriptForTest('Jenkinsfile.nightly')
+        assert Jenkinsfile != null
+    }
+
+	def '[Jenkinsfile.nightly] constructKey: no prefix' () {
 		when:
-			def Jenkinsfile = loadPipelineScriptForTest('Jenkinsfile.nightly')
+			String paramId = 'paramId'
+			String key = Jenkinsfile.constructKey('', paramId)
 		then:
-			Jenkinsfile != null
+			key == paramId
+	}
+
+	def '[Jenkinsfile.nightly] constructKey: prefix' () {
+		when:
+			String prefix = 'prefix'
+			String paramId = 'paramId'
+			String key = Jenkinsfile.constructKey(prefix, paramId)
+		then:
+			key == "${prefix}_${paramId}" 
 	}
 }
