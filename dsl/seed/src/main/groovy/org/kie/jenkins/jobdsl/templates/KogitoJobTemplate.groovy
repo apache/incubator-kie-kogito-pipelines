3876155131b88c61c7722b3aa8d96e54c3517a74
Tristan Radisson <tristan.radisson@gmail.com>
2021-02-11 16:05:05 +0100

    Updated bdd name (#114)
    * Updated BDD name
    
    * updated PR tpl back to linux

--- dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/templates/KogitoJobTemplate.groovy
+++ dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/templates/KogitoJobTemplate.groovy
@@ -128,7 +128,7 @@ class KogitoJobTemplate {
                     excludedRegions('')
                     extensions {
                         ghprbSimpleStatus {
-                            commitStatusContext(jobParams.pr.commitContext ?: 'Build&Test')
+                            commitStatusContext(jobParams.pr.commitContext ?: 'Linux')
                             addTestResults(true)
                             showMatrixStatus(false)
                             statusUrl('${BUILD_URL}display/redirect')
@@ -139,11 +139,11 @@ class KogitoJobTemplate {
                             messages {
                                 ghprbBuildResultMessage {
                                     result('ERROR')
-                                    message("The ${jobParams.pr.commitContext ?: 'Build&Test'} check has **an error**. Please check [the logs](" + '${BUILD_URL}' + 'console).')
+                                    message("The ${jobParams.pr.commitContext ?: 'Linux'} check has **an error**. Please check [the logs](" + '${BUILD_URL}' + 'console).')
                                 }
                                 ghprbBuildResultMessage {
                                     result('FAILURE')
-                                    message("The ${jobParams.pr.commitContext ?: 'Build&Test'} check has **failed**. Please check [the logs](" + '${BUILD_URL}' + 'console).')
+                                    message("The ${jobParams.pr.commitContext ?: 'Linux'} check has **failed**. Please check [the logs](" + '${BUILD_URL}' + 'console).')
                                 }
                             }
                         }
