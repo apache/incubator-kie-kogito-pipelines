6a3e356dea710ea4fa297fca855403a0ff536c5a
Radovan Synek <rsynek@redhat.com>
2021-03-16 16:47:04 +0100

    PLANNER-2363 Create the 'other' folder (#134)

--- dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/KogitoConstants.groovy
+++ dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/KogitoConstants.groovy
@@ -5,6 +5,6 @@ class KogitoConstants {
   static String KOGITO_DSL_RELEASE_FOLDER = 'release'
   static String KOGITO_DSL_TOOLS_FOLDER = 'tools'
   static String KOGITO_DSL_PULLREQUEST_FOLDER = 'pullrequest'
-
+  static String KOGITO_DSL_OTHER_FOLDER = 'other'
   static String KOGITO_DSL_RUNTIMES_BDD_FOLDER = 'kogito-runtimes.bdd'
 }
