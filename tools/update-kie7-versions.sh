radtriste/kogito-pipelines

Update Tools Scripts

tools/update-kie7-versions.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -euo pipefail
 
+GITHUB_URL="https://github.com/"
+GITHUB_URL_SSH="git@github.com:"
+
 BRANCH=master
 DEFAULT_BRANCH=master
 DRY_RUN=false
@@ -11,8 +14,9 @@ usage() {
     echo 'Usage: update-kie7-versions.sh -s $KIE_VERSION -b $BASE_BRANCH -f $FORK [-n]'
     echo
     echo 'Options:'
-    echo '  -s $KIE_VERSION  set version'
+    echo '  -v $KIE_VERSION  set version'
     echo '  -b $BASE_BRANCH  should be main or a version branch'
+    echo '  -s               Use SSH to connect to GitHub'
     echo '  -f $FORK         GH account where the branch should be pushed'
     echo '  -n               no execution: clones, creates the branch, but will not push or create the PR'
     echo
@@ -21,11 +25,11 @@ usage() {
     echo '  #  - Base branch is master'
     echo '  #  - Push the branch to evacchi/quarkus-platform'
     echo '  #  - Dry Run '
-    echo '  sh update-kie7-versions.sh -s 7.54.0.Final -b master -f evacchi -n'
+    echo '  sh update-kie7-versions.sh -v 7.54.0.Final -b master -f evacchi -n'
     echo
 }
 
-args=`getopt s:b:f:nh $*`
+args=`getopt v:b:s:f:nh $*`
 if [ $? != 0 ]
 then
         usage
@@ -42,6 +46,9 @@ do
                 -b)
                         BRANCH=$2
                         shift;shift ;;
+                -s)     
+                        GITHUB_URL=${GITHUB_URL_SSH}
+                        shift;;
                 -f)
                         FORK=$2
                         shift;shift ;;
@@ -93,7 +100,7 @@ echo
 fi
 
 
-git clone https://github.com/$ORIGIN
+git clone ${GITHUB_URL}${ORIGIN}
 cd $REPO
 
 git checkout $BRANCH
@@ -113,7 +120,7 @@ git commit -am "[$BRANCH] Bump KIE $KIE_VERSION"
  
 if [ "$DRY_RUN" = "false" ]; then
     # push the branch to a remote
-    git push -u https://github.com/$PR_FORK $PR_BRANCH
+    git push -u ${GITHUB_URL}${PR_FORK} ${PR_BRANCH}
     
     # Open a PR to kogito-runtimes using the commit as a title
     # e.g. see https://github.com/kiegroup/kogito-runtimes/pull/1200

tools/update-quarkus-versions.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -euo pipefail
  
+GITHUB_URL="https://github.com/"
+GITHUB_URL_SSH="git@github.com:"
+
 MAVEN_VERSION=3.6.2
 PROJECT=kogito
 
@@ -9,8 +12,9 @@ usage() {
     echo
     echo 'Options:'
     echo '  -p $PROJECT          set kogito or optaplanner -- default is kogito'
-    echo '  -s $QUARKUS_VERSION  set version'
+    echo '  -q $QUARKUS_VERSION  set version'
     echo '  -m $MAVEN_VERSION    set version'
+    echo '  -s                   Use SSH to connect to GitHub'
     echo '  -b $BASE_BRANCH      should be main or a version branch'
     echo '  -f $FORK             GH account where the branch should be pushed'
     echo '  -n                   no execution: clones, creates the branch, but will not push or create the PR'
@@ -21,11 +25,11 @@ usage() {
     echo '  #  - Base branch is master'
     echo '  #  - Push the branch to evacchi/quarkus-platform'
     echo '  #  - Dry Run '
-    echo '  sh update-quarkus-versions.sh -p kogito -s 2.0.0.Final -m 3.6.2 -b master -f evacchi -n'
+    echo '  sh update-quarkus-versions.sh -p kogito -q 2.0.0.Final -m 3.6.2 -b master -f evacchi -n'
     echo
 }
 
-args=`getopt p:s:b:f:m:nh $*`
+args=`getopt p:q:m:s:b:f:nh $*`
 if [ $? != 0 ]
 then
         usage
@@ -39,12 +43,15 @@ do
                 -p)
                         PROJECT=$2;
                         shift;shift ;;
-                -s)
+                -q)
                         QUARKUS_VERSION=$2;
                         shift;shift ;;
                 -m)
                         MAVEN_VERSION=$2
                         shift;shift ;;
+                -s)     
+                        GITHUB_URL=${GITHUB_URL_SSH}
+                        shift;;
                 -b)
                         BRANCH=$2
                         shift;shift ;;
@@ -117,7 +124,7 @@ fi
 # print all commands
 set -x
 
-git clone https://github.com/$ORIGIN
+git clone ${GITHUB_URL}${ORIGIN}
 cd $REPO
 
 # create branch named like version
@@ -150,7 +157,7 @@ git commit -am "Bump Quarkus $QUARKUS_VERSION"
 
 if [ "$DRY_RUN" = "false" ]; then
    # push the branch to a remote
-   git push -u https://github.com/$PR_FORK $PR_BRANCH
+   git push -u ${GITHUB_URL}${PR_FORK} ${PR_BRANCH}
    
    # Open a PR to kogito-runtimes using the commit as a title
    # e.g. see https://github.com/kiegroup/kogito-runtimes/pull/1200

apache/incubator-kie-kogito-pipelines

tools/update-kie7-versions.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -euo pipefail
 
+GITHUB_URL="https://github.com/"
+GITHUB_URL_SSH="git@github.com:"
+
 BRANCH=main
 DEFAULT_BRANCH=main
 DRY_RUN=false
@@ -11,8 +14,9 @@ usage() {
     echo 'Usage: update-kie7-versions.sh -s $KIE_VERSION -b $BASE_BRANCH -f $FORK [-n]'
     echo
     echo 'Options:'
-    echo '  -s $KIE_VERSION  set version'
+    echo '  -v $KIE_VERSION  set version'
     echo '  -b $BASE_BRANCH  should be main or a version branch'
+    echo '  -s               Use SSH to connect to GitHub'
     echo '  -f $FORK         GH account where the branch should be pushed'
     echo '  -n               no execution: clones, creates the branch, but will not push or create the PR'
     echo
@@ -21,11 +25,11 @@ usage() {
     echo '  #  - Base branch is main'
     echo '  #  - Push the branch to evacchi/quarkus-platform'
     echo '  #  - Dry Run '
-    echo '  sh update-kie7-versions.sh -s 7.54.0.Final -b main -f evacchi -n'
+    echo '  sh update-kie7-versions.sh -v 7.54.0.Final -b main -f evacchi -n'
     echo
 }
 
-args=`getopt s:b:f:nh $*`
+args=`getopt v:b:s:f:nh $*`
 if [ $? != 0 ]
 then
         usage
@@ -42,6 +46,9 @@ do
                 -b)
                         BRANCH=$2
                         shift;shift ;;
+                -s)     
+                        GITHUB_URL=${GITHUB_URL_SSH}
+                        shift;;
                 -f)
                         FORK=$2
                         shift;shift ;;
@@ -93,7 +100,7 @@ echo
 fi
 
 
-git clone https://github.com/$ORIGIN
+git clone ${GITHUB_URL}${ORIGIN}
 cd $REPO
 
 git checkout $BRANCH
@@ -113,7 +120,7 @@ git commit -am "[$BRANCH] Bump KIE $KIE_VERSION"
  
 if [ "$DRY_RUN" = "false" ]; then
     # push the branch to a remote
-    git push -u https://github.com/$PR_FORK $PR_BRANCH
+    git push -u ${GITHUB_URL}${PR_FORK} ${PR_BRANCH}
     
     # Open a PR to kogito-runtimes using the commit as a title
     # e.g. see https://github.com/kiegroup/kogito-runtimes/pull/1200

tools/update-quarkus-versions.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -euo pipefail
  
+GITHUB_URL="https://github.com/"
+GITHUB_URL_SSH="git@github.com:"
+
 MAVEN_VERSION=3.6.2
 PROJECT=kogito
 
@@ -9,8 +12,9 @@ usage() {
     echo
     echo 'Options:'
     echo '  -p $PROJECT          set kogito or optaplanner -- default is kogito'
-    echo '  -s $QUARKUS_VERSION  set version'
+    echo '  -q $QUARKUS_VERSION  set version'
     echo '  -m $MAVEN_VERSION    set version'
+    echo '  -s                   Use SSH to connect to GitHub'
     echo '  -b $BASE_BRANCH      should be main or a version branch'
     echo '  -f $FORK             GH account where the branch should be pushed'
     echo '  -n                   no execution: clones, creates the branch, but will not push or create the PR'
@@ -21,11 +25,11 @@ usage() {
     echo '  #  - Base branch is main'
     echo '  #  - Push the branch to evacchi/quarkus-platform'
     echo '  #  - Dry Run '
-    echo '  sh update-quarkus-versions.sh -p kogito -s 2.0.0.Final -m 3.6.2 -b main -f evacchi -n'
+    echo '  sh update-quarkus-versions.sh -p kogito -q 2.0.0.Final -m 3.6.2 -b main -f evacchi -n'
     echo
 }
 
-args=`getopt p:s:b:f:m:nh $*`
+args=`getopt p:q:m:s:b:f:nh $*`
 if [ $? != 0 ]
 then
         usage
@@ -39,12 +43,15 @@ do
                 -p)
                         PROJECT=$2;
                         shift;shift ;;
-                -s)
+                -q)
                         QUARKUS_VERSION=$2;
                         shift;shift ;;
                 -m)
                         MAVEN_VERSION=$2
                         shift;shift ;;
+                -s)     
+                        GITHUB_URL=${GITHUB_URL_SSH}
+                        shift;;
                 -b)
                         BRANCH=$2
                         shift;shift ;;
@@ -117,7 +124,7 @@ fi
 # print all commands
 set -x
 
-git clone https://github.com/$ORIGIN
+git clone ${GITHUB_URL}${ORIGIN}
 cd $REPO
 
 # create branch named like version
@@ -150,7 +157,7 @@ git commit -am "Bump Quarkus $QUARKUS_VERSION"
 
 if [ "$DRY_RUN" = "false" ]; then
    # push the branch to a remote
-   git push -u https://github.com/$PR_FORK $PR_BRANCH
+   git push -u ${GITHUB_URL}${PR_FORK} ${PR_BRANCH}
    
    # Open a PR to kogito-runtimes using the commit as a title
    # e.g. see https://github.com/kiegroup/kogito-runtimes/pull/1200