2d0df4982ea36f4819880552dd15747fa5374d43
radtriste <tradisso@redhat.com>
2021-09-17 17:17:48 +0200

    BXMSPROD-1478 Update Operator-sdk/Golang versions

---
+++ kogito-cloud-operator-jenkins-slave/modules/golang-installer/1.16.x/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -e
+tar -C /usr/local/ -xf  /tmp/artifacts/go1.16.linux-amd64.tar.gz
---
+++ kogito-cloud-operator-jenkins-slave/modules/operator-sdk/v1.11.x/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+chmod +x /tmp/artifacts/operator-sdk-v1.11.0-x86_64-linux-gnu 
+mkdir -p /usr/local/bin/
+cp /tmp/artifacts/operator-sdk-v1.11.0-x86_64-linux-gnu /usr/local/bin/operator-sdk
+
