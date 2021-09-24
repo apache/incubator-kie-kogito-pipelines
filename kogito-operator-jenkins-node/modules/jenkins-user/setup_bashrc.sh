b5a9a1d8d34a6ac36bdff142d45b7d1cda6da784
radtriste <tradisso@redhat.com>
2021-09-24 14:19:36 +0200

    update

--- kogito-operator-jenkins-node/modules/jenkins-user/setup_bashrc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo 'export PATH=$GOROOT/bin:$PATH' >> /home/jenkins/.bashrc
-echo 'export GOPATH=$(go env GOPATH)' >> /home/jenkins/.bashrc
-echo 'export PATH=$GOPATH/bin:$PATH' >> /home/jenkins/.bashrc
