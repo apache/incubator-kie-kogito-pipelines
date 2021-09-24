1a85779479dcc5bd54af19ded1aa0b92681820f5
radtriste <tradisso@redhat.com>
2021-09-24 16:46:09 +0200

    setup podman

--- kogito-operator-jenkins-node/modules/podman/updateConfig.sh
+++ kogito-operator-jenkins-node/modules/podman/updateConfig.sh
@@ -4,4 +4,4 @@ set -e
 mkdir -p /etc/containers/
 cp /tmp/artifacts/containers.conf  /etc/containers/containers.conf
 
-# sed -i 's|,metacopy=on||g' /etc/containers/storage.conf
+sed -i 's|,metacopy=on||g' /etc/containers/storage.conf
