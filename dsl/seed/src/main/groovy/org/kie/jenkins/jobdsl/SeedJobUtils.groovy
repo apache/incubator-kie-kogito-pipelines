5c0b4070bbd7a680cff62aa03874ef524c6cf0dd
Tristan Radisson <tristan.radisson@gmail.com>
2022-04-25 10:38:04 +0200

    Correct Repo seeds (#428)

--- dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/SeedJobUtils.groovy
+++ dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/SeedJobUtils.groovy
@@ -1,11 +1,7 @@
 package org.kie.jenkins.jobdsl
 
-import groovy.json.JsonOutput
-
-import org.kie.jenkins.jobdsl.templates.KogitoJobTemplate
 import org.kie.jenkins.jobdsl.KogitoConstants
 import org.kie.jenkins.jobdsl.Utils
-import org.kie.jenkins.jobdsl.VersionUtils
 
 /**
 * Seed Job utils
@@ -14,11 +10,11 @@ import org.kie.jenkins.jobdsl.VersionUtils
 **/
 class SeedJobUtils {
 
-    static def createSeedJobTrigger(def script, String jobName, String repository, String gitAuthor, String gitBranch, List pathsToListen, String jobRelativePathToTrigger) {
+    static def createSeedJobTrigger(def jenkinsScript, String jobName, String repository, String gitAuthor, String gitBranch, List pathsToListen, String jobRelativePathToTrigger) {
         if (pathsToListen.isEmpty()) {
             throw new RuntimeException('pathsToListen cannot be empty, else it would end up in an infinite loop ...');
         }
-        def job = script.pipelineJob(jobName) {
+        def job = jenkinsScript.pipelineJob(jobName) {
             description('This job listens to pipelines repo and launch the seed job if needed. DO NOT USE FOR TESTING !!!! See https://github.com/kiegroup/kogito-pipelines/blob/main/docs/jenkins.md#test-specific-jobs')
 
             logRotator {
@@ -30,10 +26,17 @@ class SeedJobUtils {
             }
 
             parameters {
+                stringParam('SEED_AUTHOR', Utils.getSeedAuthor(jenkinsScript), 'If different from the default')
+                stringParam('SEED_BRANCH', Utils.getSeedBranch(jenkinsScript), 'If different from the default')
+
                 booleanParam('FORCE_REBUILD', false, 'Default, the job will scan for modified files and do the update in case some files are modified. In case you want to force the DSL generation')
             }
 
             environmentVariables {
+                env('SEED_REPO', KogitoConstants.KOGITO_PIPELINES_REPOSITORY)
+                env('SEED_SCRIPTS_FILEPATH', 'dsl/seed/jobs/scripts/seed_repo_generation.groovy')
+                env('JOB_TYPE', 'TRIGGER')
+
                 env('JOB_RELATIVE_PATH_TO_TRIGGER', jobRelativePathToTrigger)
                 env('LISTEN_TO_MODIFIED_PATHS', new groovy.json.JsonBuilder(pathsToListen).toString())
             }
@@ -52,7 +55,11 @@ class SeedJobUtils {
                             }
                         }
                     }
-                    scriptPath('dsl/seed/jobs/Jenkinsfile.seed.trigger')
+                    if (repository == KogitoConstants.KOGITO_PIPELINES_REPOSITORY) {
+                        scriptPath('dsl/seed/jobs/Jenkinsfile.seed.trigger')
+                    } else {
+                        scriptPath('.ci/jenkins/dsl/Jenkinsfile.seed')
+                    }
                 }
             }
 
@@ -67,8 +74,7 @@ class SeedJobUtils {
             }
         }
         // Trigger jobs need to be executed once for the hook to work ...
-        // There will 
-        script.queue(jobName)
+        jenkinsScript.queue(jobName)
         return job
     }
 }
