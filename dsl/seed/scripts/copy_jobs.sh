d5ec0e8a3c0daecc2ff39b96cc1697337b1d5097
radtriste <tradisso@redhat.com>
2022-05-05 11:57:15 +0200

    KOGITO-7101 Correct DSL tests

--- dsl/seed/scripts/copy_jobs.sh
+++ dsl/seed/scripts/copy_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-script_dir_path=`dirname "${BASH_SOURCE[0]}"`
+script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
 seed_dir="${script_dir_path}/.."
 
-echo '----- Copying jobs.groovy for testing'
+echo "----- Copying jobs.groovy for testing from $(pwd)"
 cp jobs.groovy ${seed_dir}/jobs/
