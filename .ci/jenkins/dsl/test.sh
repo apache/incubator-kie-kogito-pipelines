8a45e65c0b9228a81bd0f47fbae5b63f9bf46878
radtriste <tradisso@redhat.com>
2022-06-28 15:33:56 +0200

    updated seed_test script

--- .ci/jenkins/dsl/test.sh
+++ .ci/jenkins/dsl/test.sh
@@ -2,4 +2,8 @@
 
 SCRIPT_DIR_PATH=`dirname "${BASH_SOURCE[0]}"`
 
+export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO=kiegroup/kogito-pipelines
+export DSL_DEFAULT_MAIN_CONFIG_FILE_REF=main
+export DSL_DEFAULT_MAIN_CONFIG_FILE_PATH=.ci/jenkins/config/main.yaml
+
 ${SCRIPT_DIR_PATH}/../../../dsl/seed/scripts/seed_test.sh $@ 
