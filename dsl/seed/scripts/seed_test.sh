3c9670ed3910e3d2692e1f5491b021504c25a360
Tristan Radisson <tristan.radisson@gmail.com>
2022-07-06 09:50:18 +0200

    Update release pipeline to tag only (#531)

--- dsl/seed/scripts/seed_test.sh
+++ dsl/seed/scripts/seed_test.sh
@@ -12,7 +12,7 @@
 
 script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
 
-TEMP_DIR=`mktemp -d`
+pipelines_final_dir=`mktemp -d`
 GIT_SERVER='github.com'
 
 usage() {
@@ -32,16 +32,25 @@ usage() {
     echo '  -f MAIN_CONFIG_FILE_REF       Main config file branch to the MAIN_CONFIG_REPO. Default can be given via `DSL_DEFAULT_MAIN_CONFIG_FILE_REF` env var'
     echo '  -g MAIN_CONFIG_FILE_PATH      Path to the test config file in MAIN_CONFIG_REPO. If no MAIN_CONFIG_REPO is given, then it will look locally. Default can be given via `DSL_DEFAULT_MAIN_CONFIG_FILE_PATH` env var'
     echo '  -c BRANCH_CONFIG_FILE_PATH    Local Path to the branch config file. `MAIN_CONFIG_*` will be ignored if that one is given. Default can be given via `DSL_DEFAULT_BRANCH_CONFIG_FILE_PATH` env var'
-    echo '  -d BRANCH_CONFIG_BRANCH       Base branch to checkout the branch config. Default is `TARGET_BRANCH` or via `DSL_DEFAULT_BRANCH_CONFIG_BRANCH` env var'
+    echo '  -d BRANCH_CONFIG_NAME         Base branch to checkout the branch config. Default is `TARGET_BRANCH` or via `DSL_DEFAULT_BRANCH_CONFIG_NAME` or `DSL_DEFAULT_BRANCH_CONFIG_BRANCH` (this latter one is deprecated) env var'
     echo '  PATH                          Path to test'
     echo
 }
 
+# checkout_repository $OUTPUT_DIR $OWNER $REPOSITORY $BRANCH $TARGER_OWNER $TARGET_REPOSITORY $TARGET_BRANCH
 checkout_repository() {
   local output_dir=$1
 
-  echo "----- Cloning ${git_server}${owner}/${repository} repo on branch ${branch}"
-  git clone --single-branch --depth 1 --branch ${branch} ${git_server}${owner}/${repository} ${output_dir}
+  local owner=$2
+  local repository=$3
+  local branch=$4
+
+  local target_owner=$5
+  local target_repository=$6
+  local target_branch=$7
+
+  echo "----- Cloning ${git_server_url}${owner}/${repository} repo on branch ${branch}"
+  git clone --single-branch --depth 1 --branch ${branch} ${git_server_url}${owner}/${repository} ${output_dir}
   clone_status=$?
 
   set -e
@@ -51,15 +60,41 @@ checkout_repository() {
       echo 'No target repository given. Cannot retrieve repository ...'
       exit 1
     else 
-      echo "[WARN] Error cloning ${git_server}${target_owner}/${target_repository} on branch ${target_branch}"
-      echo "----- Cloning ${git_server}${target_owner}/${target_repository} on branch ${target_branch}"
-      git clone --single-branch --depth 1 --branch ${target_branch} ${git_server}${target_owner}/${target_repository} ${output_dir}
+      echo "[WARN] Error cloning ${git_server_url}${target_owner}/${target_repository} on branch ${target_branch}"
+      echo "----- Cloning ${git_server_url}${target_owner}/${target_repository} on branch ${target_branch}"
+      git clone --single-branch --depth 1 --branch ${target_branch} ${git_server_url}${target_owner}/${target_repository} ${output_dir}
     fi
   else
     echo "Clone succeeded"
   fi
 
   set +e
+}
+
+installYqIfNeeded() {
+  yq_path="$(command -v yq)"
+  if [[ ! $(command -v yq) ]]; then
+    echo 'yq command cannot be found ... Installing it locally'
+    yq_path="$(mktemp -d)/yq"
+    wget https://github.com/mikefarah/yq/releases/download/v4.25.1/yq_linux_amd64.tar.gz -O - |  tar xz && ls -al && mv yq_linux_amd64 "${yq_path}"
+    yq --version
+
+    export PATH="${PATH}:${yq_path}"
+  fi
+}
+
+retrieveMainConfigFile() {
+  if [ ! -z ${main_config_file_repo} ]; then
+    echo 'Retrieve main config file from repository'
+    main_config_repo_path=$(mktemp -d)
+    main_config_file_path="${main_config_repo_path}/${main_config_file_path}"
+    mkdir -p "${main_config_repo_path}"
+    
+    local owner=$(echo ${main_config_file_repo} | awk -F/ '{print $1}')
+    local repository=$(echo ${main_config_file_repo} | awk -F/ '{print $2}' | awk -F'.' '{print $1}')
+    local branch=${main_config_file_ref}
+    checkout_repository "${main_config_repo_path}" "${owner}" "${repository}" "${branch}"
+  fi
 }
 
 full_repository=${DSL_DEFAULT_REPOSITORY}
@@ -73,7 +108,7 @@ main_config_file_repo="${DSL_DEFAULT_MAIN_CONFIG_FILE_REPO}"
 main_config_file_ref="${DSL_DEFAULT_MAIN_CONFIG_FILE_REF}"
 main_config_file_path="${DSL_DEFAULT_MAIN_CONFIG_FILE_PATH}"
 branch_config_file_path="${DSL_DEFAULT_BRANCH_CONFIG_FILE_PATH}"
-branch_config_branch="${DSL_DEFAULT_BRANCH_CONFIG_BRANCH}"
+branch_config_name="${DSL_DEFAULT_BRANCH_CONFIG_NAME:-${DSL_DEFAULT_BRANCH_CONFIG_BRANCH}}" # To remove `DSL_DEFAULT_BRANCH_CONFIG_BRANCH` at some point 
 
 while getopts "r:o:h:t:b:p:e:f:g:c:d:h" i
 do
@@ -89,7 +124,7 @@ do
         f) main_config_file_ref=${OPTARG} ;;
         g) main_config_file_path=${OPTARG} ;;
         c) branch_config_file_path=${OPTARG} ;;
-        d) branch_config_branch=${OPTARG} ;;
+        d) branch_config_name=${OPTARG} ;;
         h) usage; exit 0 ;;
         \?) usage; exit 1 ;;
     esac
@@ -102,18 +137,18 @@ if [ ! -z "$1" ]; then
   shift
 fi
 
-if [ -z ${branch_config_branch} ]; then
-  branch_config_branch=${target_branch}
+if [ -z ${branch_config_name} ]; then
+  branch_config_name=${target_branch}
 fi
 
 git_url=$(git remote -v | grep origin | awk -F' ' '{print $2}' | head -n 1)
 echo "Got Git URL = ${git_url}"
 
-git_server=
+git_server_url=
 if [[ "${git_url}" = https://* ]]; then
-  git_server="https://${GIT_SERVER}/"
+  git_server_url="https://${GIT_SERVER}/"
 elif [[ "${git_url}" = git@* ]]; then 
-  git_server="git@${GIT_SERVER}:"
+  git_server_url="git@${GIT_SERVER}:"
 else
   echo "Unknown protocol for url ${git_url}"
   exit 1
@@ -126,7 +161,7 @@ fi
 
 # parse full repository
 if [ -z "${full_repository}" ]; then
-  full_repository=$(echo ${git_url} | awk -F"${git_server}" '{print $2}')
+  full_repository=$(echo ${git_url} | awk -F"${git_server_url}" '{print $2}')
 fi
 owner=$(echo ${full_repository} | awk -F/ '{print $1}')
 if [ -z "${repository}" ]; then
@@ -143,7 +178,7 @@ else
   fi
 fi
 
-echo "git_server...............${git_server}"
+echo "git_server_url...........${git_server_url}"
 echo "owner....................${owner}"
 echo "repository...............${repository}"
 echo "branch...................${branch}"
@@ -154,17 +189,24 @@ echo "pipelines_repo...........${pipelines_repo}"
 echo "main_config_file_repo....${main_config_file_repo}"
 echo "main_config_file_ref.....${main_config_file_ref}"
 echo "main_config_file_path....${main_config_file_path}"
-echo "branch_config_branch.....${branch_config_branch}"
+echo "branch_config_name.......${branch_config_name}"
 echo "branch_config_file_path..${branch_config_file_path}"
 
-current_repository="$(echo ${git_url}  | awk -F"${git_server}" '{print $2}' | awk -F'/' '{print $2}' | awk -F'.' '{print $1}')"
-echo "Got current repository = ${current_repository}"
+installYqIfNeeded
+
+retrieveMainConfigFile
+
+echo '----- Read main project'
+main_project=$(yq '.ecosystem.main_project' ${main_config_file_path})
+echo "Use main project ${main_project}"
 
 # retrieve repository
+current_repository="$(echo ${git_url}  | awk -F"${git_server_url}" '{print $2}' | awk -F'/' '{print $2}' | awk -F'.' '{print $1}')"
+echo "Got current repository = ${current_repository}"
 if [ "${repository}" != "${current_repository}" ]; then
   echo "Current repository is not the right one, checking out ${repository}"
   repo_tmp_dir=`mktemp -d`
-  checkout_repository "${repo_tmp_dir}"
+  checkout_repository "${repo_tmp_dir}" "${owner}" "${repository}" "${branch}" "${target_owner}" "${target_repository}" "${target_branch}"
   echo "Going into ${repository} directory"
   cd ${repo_tmp_dir}
 fi
@@ -177,87 +219,67 @@ fi
 pipelines_repo_path=
 if [ "${repository}" = 'kogito-pipelines' ]; then
   echo '----- Copying seed repo'
-  mkdir -p ${TEMP_DIR}/dsl
+  mkdir -p ${pipelines_final_dir}/dsl
   pipelines_repo_path="${script_dir_path}/../../.."
-  cp -r ${pipelines_repo_path}/dsl/seed ${TEMP_DIR}/dsl
+  cp -r ${pipelines_repo_path}/dsl/seed ${pipelines_final_dir}/dsl
 else
   if [ -z ${pipelines_repo} ]; then
-    repository='kogito-pipelines'
-    target_repository='kogito-pipelines'
-    checkout_repository "${TEMP_DIR}"
-    pipelines_repo_path="${TEMP_DIR}"
+    echo "----- Checkout Pipelines repo"
+    seed_branch=${target_branch}
+    regex_release_branch="^[0-9]+\.[0-9]+\.x$"
+    regex_release_branch_with_project="^${main_project}-[0-9]+\.[0-9]+\.x$"
+    if [[ ${seed_branch} =~ ${regex_release_branch} ]]; then 
+      seed_branch="seed-${main_project}-${seed_branch}"
+    elif [[ ${seed_branch} =~ ${regex_release_branch_with_project} ]]; then 
+      seed_branch="seed-${seed_branch}"
+    fi
+    checkout_repository "${pipelines_final_dir}" "${owner}" 'kogito-pipelines' "${branch}" 'kiegroup' 'kogito-pipelines' "${seed_branch}"
+    pipelines_repo_path="${pipelines_final_dir}"
   else
     echo '----- Copying given pipelines seed repo'
-    mkdir -p ${TEMP_DIR}/dsl
+    mkdir -p ${pipelines_final_dir}/dsl
     pipelines_repo_path=${pipelines_repo}
-    cp -r ${pipelines_repo_path}/dsl/seed ${TEMP_DIR}/dsl
+    cp -r ${pipelines_repo_path}/dsl/seed ${pipelines_final_dir}/dsl
   fi
 fi
 
 if [ -z "${branch_config_file_path}" ]; then
   echo '----- Retrieve branch config file'
 
-  temp_dir=$(mktemp -d)
-  yq_path="$(command -v yq)"
-  if [[ ! $(command -v yq) ]]; then
-    echo '--------- Installing yq locally'  
-    yq_path="${temp_dir}/yq"
-    wget https://github.com/mikefarah/yq/releases/download/v4.25.1/yq_linux_amd64.tar.gz -O - |  tar xz && ls -al && mv yq_linux_amd64 "${yq_path}"
-    yq --version
-  fi
-
-  if [ ! -z ${main_config_file_repo} ]; then
-    echo '--------- Retrieve main config file'
-    main_config_repo_path="${temp_dir}/main_config_repo"
-    main_config_file_path="${main_config_repo_path}/${main_config_file_path}"
-    mkdir -p "${main_config_repo_path}"
-    
-    owner=$(echo ${main_config_file_repo} | awk -F/ '{print $1}')
-    repository=$(echo ${main_config_file_repo} | awk -F/ '{print $2}' | awk -F'.' '{print $1}')
-    branch=${main_config_file_ref}
-    checkout_repository "${main_config_repo_path}"
-  fi
-
   if [ ! -z ${main_config_file_path} ]; then
-    echo '--------- Read main project'
-    main_project=$(yq '.ecosystem.main_project' ${main_config_file_path})
-    echo "Use main project ${main_project}"
+    echo "Use branch config name ${branch_config_name}"
 
-    echo '--------- Setup branch name for reading config'
-    branch_config_file_ref="${branch_config_branch}"
-    if [[ ${branch_config_file_ref} =~ ^[0-9]+\.[0-9]+\.x$ ]]; then 
-      branch_config_file_ref="${main_project}-${branch_config_file_ref}"; 
+    echo '--------- Retrieve branch config branch'
+    branch_config_file_ref="$(yq '.git.branches[] | select(.name == "${branch_config_name}") | .seed.config_file.git.repository' ${main_config_file_path})"
+    if [ -z ${branch_config_file_ref} ]; then 
+      branch_config_file_ref="${branch_config_name}"
     fi
-    echo "Use branch config ${branch_config_file_ref}"
+    echo "Use branch config branch ${branch_config_file_ref}"
 
     echo '--------- Retrieve branch config file information'
-    branch_config_file_git_repository="$(yq '.git.branches[] | select(.name == "${branch_config_file_ref}") | .seed.config_file.git.repository' ${main_config_file_path})"
+    branch_config_file_git_repository="$(yq '.git.branches[] | select(.name == "${branch_config_name}") | .seed.config_file.git.repository' ${main_config_file_path})"
     if [ -z ${branch_config_file_git_repository} ]; then 
       branch_config_file_git_repository=$(yq '.seed.config_file.git.repository' ${main_config_file_path}); 
     fi
     echo "Use branch config file repo ${branch_config_file_git_repository}"
 
-    branch_config_file_git_author="$(yq '.git.branches[] | select(.name == "${branch_config_file_ref}") | .seed.config_file.git.author.name' ${main_config_file_path})"
+    branch_config_file_git_author="$(yq '.git.branches[] | select(.name == "${branch_config_name}") | .seed.config_file.git.author.name' ${main_config_file_path})"
     if [ -z ${branch_config_file_git_author} ]; then 
       branch_config_file_git_author=$(yq '.seed.config_file.git.author.name' ${main_config_file_path}); 
     fi
     echo "Use branch config file author ${branch_config_file_git_author}"
 
-    branch_config_file_path="$(yq '.git.branches[] | select(.name == "${branch_config_file_ref}") | .seed.config_file.path' ${main_config_file_path})"
+    branch_config_file_path="$(yq '.git.branches[] | select(.name == "${branch_config_name}") | .seed.config_file.path' ${main_config_file_path})"
     if [ -z ${branch_config_file_path} ]; then 
       branch_config_file_path=$(yq '.seed.config_file.path' ${main_config_file_path}); 
     fi
     echo "Use branch config file path ${branch_config_file_path}"
 
     echo '--------- Retrieve branch config file'
-    branch_config_repo_path="${temp_dir}/branch_config_repo"
+    branch_config_repo_path=$(mktemp -d)
     branch_config_file_path="${branch_config_repo_path}/${branch_config_file_path}"
     mkdir -p "${branch_config_repo_path}"
-    
-    owner=${branch_config_file_git_author}
-    repository=${branch_config_file_git_repository}
-    branch=${branch_config_file_ref}
-    checkout_repository "${branch_config_repo_path}"
+    checkout_repository "${branch_config_repo_path}" "${branch_config_file_git_author}" "${branch_config_file_git_repository}" "${branch_config_file_ref}"
 
     echo "Use config file ${branch_config_file_path}"
   fi
@@ -274,8 +296,8 @@ if [ -z "${branch_config_file_path}" ]; then
 fi
 
 echo "----- Copy branch config file ${branch_config_file_path}"
-cp "${branch_config_file_path}" ${TEMP_DIR}/dsl/seed/branch_config.yaml
+cp "${branch_config_file_path}" ${pipelines_final_dir}/dsl/seed/branch_config.yaml
 
-${TEMP_DIR}/dsl/seed/scripts/copy_jobs.sh
+${pipelines_final_dir}/dsl/seed/scripts/copy_jobs.sh
 
-${TEMP_DIR}/dsl/seed/scripts/test_jobs.sh
+${pipelines_final_dir}/dsl/seed/scripts/test_jobs.sh
