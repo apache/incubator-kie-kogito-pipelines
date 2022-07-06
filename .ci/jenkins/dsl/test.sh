222490f58034f0aa73e7deb9f6437d67b546aa74
radtriste <tradisso@redhat.com>
2022-07-06 11:50:36 +0200

    DSL testing: Remove Gradle warnings

--- .ci/jenkins/dsl/test.sh
+++ .ci/jenkins/dsl/test.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR_PATH=`dirname "${BASH_SOURCE[0]}"`
 
 export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO=kiegroup/kogito-pipelines
 export DSL_DEFAULT_MAIN_CONFIG_FILE_REF=main
-export DSL_DEFAULT_MAIN_CONFIG_FILE_PATH=.ci/jenkins/config/main.yaml
+export DSL_DEFAULT_MAIN_CONFIG_FILE_PATH=dsl/config/main.yaml
 
 ${SCRIPT_DIR_PATH}/../../../dsl/seed/scripts/seed_test.sh $@ 
