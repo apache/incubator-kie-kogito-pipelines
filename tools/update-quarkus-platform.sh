8038263e0f7cb60d5271be3fb54d3fe621980d45
radtriste <tradisso@redhat.com>
2022-07-12 15:08:33 +0200

    Add option to update-quarkus-platform script

--- tools/update-quarkus-platform.sh
+++ tools/update-quarkus-platform.sh
@@ -8,27 +8,38 @@ GITHUB_URL_SSH="git@github.com:"
 VERSION=
 PROJECT=kogito
 DRY_RUN=false
-BRANCH=main
+BASE_BRANCH=main
+PR_BRANCH=
+DISABLE_CHECKOUT=false
 
 usage() {
-    echo 'Usage: update-quarkus-platform.sh -v $VERSION -f $FORK [-s] [-b] [-n] COMMAND'
+    echo 'Usage: update-quarkus-platform.sh -v $VERSION [-p PROJECT] -f $FORK [-d] [-s] [-b BASE_BRANCH] [-h PR_BRANCH] [-n] COMMAND'
     echo
     echo 'Options:'
     echo '  -v $VERSION          set version'
     echo '  -p project           `kogito` or `optaplanner`. Default is kogito.'
     echo '  -f $FORK             GH account where the branch should be pushed'
+    echo '  -d                   Disable checkout. This means you are running the script in an already checked out quarkus-platform repository.'
     echo '  -s                   Use SSH to connect to GitHub'
-    echo '  -b $BRANCH           Quarkus Platform branch (optional. Default is `main`)'
+    echo '  -b $BASE_BRANCH      Quarkus Platform branch (optional. Default is `main`)'
+    echo '  -h $PR_BRANCH        Branch to use for the PR'
     echo '  -n                   no execution: clones, creates the branch, but will not push or create the PR'
     echo '  COMMAND              may be `stage` or `finalize`'
     echo
-    echo 'Examples:'#!/bin/sh
+    echo 'Examples:'
     echo '  # Stage the PR'
-    echo '  #  - Bump Kogito 1.24.0.Final
+    echo '  #  - Bump Kogito 1.24.0.Final'
     echo '  #  - Add staging repositories'
-    echo '  #  - Push the branch to evacchi/quarkus-platform'
+    echo '  #  - Commit the changes'
     echo '  #  - Dry Run'
     echo '  sh update-quarkus-platform.sh -v 1.24.0.Final -p kogito -f evacchi -n stage'
+    echo
+    echo '  # Update a current checked out repository'
+    echo '  #  - Bump Kogito 1.24.0.Final'
+    echo '  #  - Add staging repositories'
+    echo '  #  - Commit the changes'
+    echo '  #  - Push the `bump-kogito-world` branch to evacchi/quarkus-platform'
+    echo '  sh update-quarkus-platform.sh -v 1.24.0.Final -p kogito -d -f evacchi -h bump-kogito-world stage'
     echo
     echo '  # Finalize the PR:'
     echo '  #  - Remove staging repositories'
@@ -37,7 +48,7 @@ usage() {
     echo '  sh update-quarkus-platform.sh -v 1.24.0.Final -p kogito -f evacchi -n finalize'
 }
 
-args=`getopt v:p:f:b:snh $*`
+args=`getopt v:p:f:b:h:dsnh $*`
 if [ "$#" -eq 0 -o $? != 0 ]; then
     >&2 echo ERROR: no args given.
 
@@ -58,11 +69,17 @@ do
                 -f)
                         FORK=$2
                         shift;shift ;;
+                -d)     
+                        DISABLE_CHECKOUT=true
+                        shift;;
                 -s)     
                         GITHUB_URL=${GITHUB_URL_SSH}
                         shift;;
                 -b)     
-                        BRANCH=$2
+                        BASE_BRANCH=$2
+                        shift;shift ;;
+                -h)     
+                        PR_BRANCH=$2
                         shift;shift ;;
                 -n)     
                         DRY_RUN=true
@@ -80,14 +97,14 @@ do
 done
 
 
-if [ "$VERSION" = "" ]; then 
+if [ -z "$VERSION" ]; then 
     >&2 echo ERROR: no version specified.
     usage
 
     exit 2
 fi
 
-if [ "$FORK" = "" ]; then 
+if [ -z "$FORK" ]; then 
     >&2 echo ERROR: no fork specified.
     usage
 
@@ -113,13 +130,15 @@ REPO=quarkus-platform
 PR_FORK=$FORK/$REPO
 ORIGIN=quarkusio/$REPO
 
-PR_BRANCH=bump-${PROJECT}-${VERSION}
+if [ -z "$PR_BRANCH" ]; then 
+    PR_BRANCH=bump-${PROJECT}-${VERSION}
+fi
 
 
 echo GITHUB_URL...............$GITHUB_URL
 echo ORIGIN...................$ORIGIN
 echo PR_FORK..................$PR_FORK
-echo BRANCH...................$BRANCH
+echo BASE_BRANCH..............$BASE_BRANCH
 echo PR_BRANCH................$PR_BRANCH
 echo VERSION..................$VERSION
 echo COMMAND..................$COMMAND
@@ -133,51 +152,71 @@ fi
 stage() {
     set -x
 
-    git clone ${GITHUB_URL}${ORIGIN}
-    cd $REPO
+    if [ "${DISABLE_CHECKOUT}" != 'true' ]; then
+        git clone ${GITHUB_URL}${ORIGIN}
+        cd $REPO
+    fi
 
-    # ensure main branch
-    git checkout $BRANCH
-    # create branch
-    git checkout -b $PR_BRANCH
+    # ensure base branch
+    git checkout $BASE_BRANCH
+    
+    # create branch if needed
+    set +e
+    git checkout $PR_BRANCH
+    if [ "$?" != "0" ]; then
+        git checkout -b $PR_BRANCH
+    fi
     
     # add custom repositories
-    echo "$DIFF_FILE" | patch .github/mvn-settings.xml
+    cat ${MAVEN_SETTINGS_FILE} | grep ${KOGITO_STAGING_REPOSITORY}
+    if [ "$?" != "0" ]; then
+        echo "$DIFF_FILE" | patch ${MAVEN_SETTINGS_FILE}
+    fi
+    set -e
     
     # process versions
     ./mvnw \
-    -s .github/mvn-settings.xml \
+    -s ${MAVEN_SETTINGS_FILE} \
     versions:set-property \
     -Dproperty=${PROJECT}-quarkus.version \
     -DnewVersion=${VERSION} \
     -DgenerateBackupPoms=false
     
     # update pom metadata
-    ./mvnw -s .github/mvn-settings.xml -Dsync
+    ./mvnw -s ${MAVEN_SETTINGS_FILE} -Dsync
     
     # commit all
     git commit -am "${PROJECT} ${VERSION}"
 
     if [ "$DRY_RUN" = "false" ]; then
         git push -u ${GITHUB_URL}$PR_FORK $PR_BRANCH
-        gh pr create --fill --base $BRANCH -R $ORIGIN
+        gh pr create --fill --base $BASE_BRANCH -R $ORIGIN
+    else
+        echo 'Do not push/create PR as per parameters...'
     fi
 }
 
 finalize() {
     set -x
 
-    if [ -d "$REPO" ]; then
-        cd $REPO
-    else
-        git clone ${GITHUB_URL}$PR_FORK
-        cd $REPO;
+    if [ "${DISABLE_CHECKOUT}" != 'true' ]; then
+        if [ -d "$REPO" ]; then
+            cd $REPO
+        else
+            git clone ${GITHUB_URL}$PR_FORK
+            cd $REPO;
+        fi
     fi
 
     git checkout $PR_BRANCH
 
     # undo patch to add repos
-    echo "$DIFF_FILE" | patch -R .github/mvn-settings.xml
+    set +e
+    cat ${MAVEN_SETTINGS_FILE} | grep ${KOGITO_STAGING_REPOSITORY}
+    if [ "$?" = "0" ]; then
+        echo "$DIFF_FILE" | patch -R ${MAVEN_SETTINGS_FILE}
+    fi
+    set -e
 
     ./mvnw -Dsync
 
@@ -187,13 +226,17 @@ finalize() {
     if [ "$DRY_RUN" = "false" ]; then
         # push forced (we are overwriting the old commit)
         git push --force-with-lease
+    else
+        echo 'Do not push as per parameters...'
     fi
 }
 
-DIFF_FILE='diff --git a/.github/mvn-settings.xml b/.github/mvn-settings.xml
+KOGITO_STAGING_REPOSITORY='https://repository.jboss.org/nexus/content/groups/kogito-public/'
+MAVEN_SETTINGS_FILE='.github/mvn-settings.xml'
+DIFF_FILE="diff --git a/${MAVEN_SETTINGS_FILE} b/${MAVEN_SETTINGS_FILE}
 index d5e4664b..b03cc023 100644
---- a/.github/mvn-settings.xml
-+++ b/.github/mvn-settings.xml
+--- a/${MAVEN_SETTINGS_FILE}
++++ b/${MAVEN_SETTINGS_FILE}
 @@ -14,6 +14,14 @@
              <enabled>false</enabled>
            </snapshots>
@@ -204,11 +247,11 @@ index d5e4664b..b03cc023 100644
 +          </snapshots>
 +          <id>kogito</id>
 +          <name>kogito</name>
-+          <url>https://repository.jboss.org/nexus/content/groups/kogito-public/</url>
++          <url>${KOGITO_STAGING_REPOSITORY}</url>
 +        </repository>
        </repositories>
        <pluginRepositories>
          <pluginRepository>
-'
+"
 # execute
 $COMMAND
