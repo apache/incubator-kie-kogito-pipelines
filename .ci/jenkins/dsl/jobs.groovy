770fd32e252650ea72e9deed01ea0fc125baf2d6
radtriste <tradisso@redhat.com>
2022-10-14 17:03:51 +0200

    correction

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -189,7 +189,6 @@ void setupUpdateQuarkusPlatformJob() {
             BUILD_BRANCH_NAME: "${GIT_BRANCH}",
             QUARKUS_PLATFORM_BRANCH_NAME: 'main',
             PROJECT_NAME: 'kogito',
-            NOTIFICATION_JOB_NAME: 'Kogito',
             FORK_GIT_AUTHOR: "${GIT_FORK_AUTHOR}",
             FORK_GIT_AUTHOR_CREDS_ID: "${GIT_FORK_AUTHOR_CREDENTIALS_ID}",
     ])
