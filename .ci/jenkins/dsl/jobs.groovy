95a998101dcc0b36a502f0dbf8bea8ecd111e63d
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2022-11-30 09:45:47 +0100

    new pipeline job for tagging/bumping up kogito-docs

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -30,6 +30,7 @@ setupNightlyJob()
 
 // Release
 setupReleaseJob()
+setupDocsJob()
 
 /////////////////////////////////////////////////////////////////
 // Methods
@@ -200,4 +201,23 @@ void setupReleaseJob() {
 void setupBuildOperatorNode() {
     def jobParams = KogitoJobUtils.getBasicJobParams(this, 'build-operator-node', Folder.TOOLS, "${JENKINSFILE_PATH}/Jenkinsfile.build-operator-node")
     KogitoJobTemplate.createPipelineJob(this, jobParams)
+}
+
+void setupDocsJob() {
+    def jobParams = KogitoJobUtils.getBasicJobParams(this, 'kogito-docs', Folder.RELEASE, "${JENKINSFILE_PATH}/Jenkinsfile.ecosystem.update-kogito-docs", "Update Kogito docs")
+    KogitoJobUtils.setupJobParamsDefaultMavenConfiguration(this, jobParams)
+    jobParams.env.putAll([
+            JENKINS_EMAIL_CREDS_ID: "${JENKINS_EMAIL_CREDS_ID}",
+            GIT_AUTHOR: "${GIT_AUTHOR_NAME}",
+            GIT_AUTHOR_CREDENTIALS_ID: "${GIT_AUTHOR_CREDENTIALS_ID}",
+            SSH_KOGITO_DOCS_PATH: "${FILEMGMT_KOGITO_DOCS_SFTP_URL}",
+            RSYNC_KOGITO_DOCS_PATH: "${FILEMGMT_KOGITO_DOCS_RSYNC_URL}",
+    ])
+    KogitoJobTemplate.createPipelineJob(this, jobParams)?.with {
+        parameters {
+            stringParam('KOGITO_DOCS_VERSION', '', 'Kogito docs version to release as Major.minor.micro')
+            booleanParam('BUMP_UP',false,'Is a new KIE version needed?')
+            stringParam('KIE_VERSION','','KIE version to bump up to as Major.minor.micro-SNAPSHOT')
+        }
+    }
 }
