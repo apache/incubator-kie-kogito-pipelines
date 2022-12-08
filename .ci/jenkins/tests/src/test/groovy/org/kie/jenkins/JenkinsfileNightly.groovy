cc91edb0a56d97ef56ce8ef8f5035def6a3c8608
radtriste <tradisso@redhat.com>
2022-12-08 09:23:19 +0100

    Updated tests

--- .ci/jenkins/tests/src/test/groovy/org/kie/jenkins/JenkinsfileNightly.groovy
+++ .ci/jenkins/tests/src/test/groovy/org/kie/jenkins/JenkinsfileNightly.groovy
@@ -18,11 +18,12 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
     @Before
     void setUp() throws Exception {
         super.setUp()
-        mockSharedLibraries('jenkins-pipeline-shared-libraries')
 
         addParam('SKIP_TESTS', false)
         addParam('SKIP_INTEGRATION_TESTS', true)
         addEnvVar('GIT_BRANCH_NAME', 'BRANCH')
+        addEnvVar('GIT_AUTHOR', 'AUTHOR')
+        addEnvVar('GIT_AUTHOR_CREDS_ID', 'AUTHOR_CREDS_ID')
         addEnvVar('JENKINS_EMAIL_CREDS_ID', 'KOGITO_CI_EMAIL_TO')
         addEnvVar('STAGE_NAME', 'STAGE_NAME')
         addEnvVar('BUILD_NUMBER', 'BUILD_NUMBER')
@@ -32,8 +33,13 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
 
         helper.registerAllowedMethod('credentials', [String.class], { str -> return str })
         helper.registerAllowedMethod('emailext', [Map.class], { str -> return 'emailext' })
-    }
 
+        mockSharedLibraries('jenkins-pipeline-shared-libraries')
+        mockSharedLibVarsCall('githubscm', 'resolveRepository', { repo, author, branch, ignoreErrors, credsId -> registerTestCallstack('githubscm.resolveRepository', "${repo}, ${author}, ${branch}, ${ignoreErrors}, ${credsId}") })
+        mockSharedLibVarsCall('githubscm', 'createBranch', { branch -> registerTestCallstack('githubscm.createBranch', "${branch}") })
+        mockSharedLibVarsCall('githubscm', 'pushObject', { remote, branch, credsId -> registerTestCallstack('githubscm.pushObject', "${remote}, ${branch}, ${credsId}") })
+
+    }
     @Test
     void default_execution() throws Exception {
         helper.registerAllowedMethod('build', [Map.class], { map -> return [
@@ -47,8 +53,11 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
             'drools' : [:],
             'kogito-runtimes' : [:],
             'kogito-apps' : [:],
-            'kogito-examples': [ UPDATE_NIGHTLY_BRANCH: true ],
+            'kogito-examples': [:],
         ])
+        assertTestCallstackContains('githubscm.resolveRepository', 'kogito-examples, AUTHOR, BRANCH, false, AUTHOR_CREDS_ID')
+        assertTestCallstackContains('githubscm.createBranch', 'nightly-BRANCH')
+        assertTestCallstackContains('githubscm.pushObject', 'origin, nightly-BRANCH, AUTHOR_CREDS_ID')
     }
 
     @Test
@@ -65,14 +74,18 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
             'drools' : [:],
             'kogito-runtimes' : [:],
             'kogito-apps' : [:],
-            'kogito-examples': [ UPDATE_NIGHTLY_BRANCH: true ],
+            'kogito-examples': [:],
         ], true)
+
+        assertTestCallstackContains('githubscm.resolveRepository', 'kogito-examples, AUTHOR, BRANCH, false, AUTHOR_CREDS_ID')
+        assertTestCallstackContains('githubscm.createBranch', 'nightly-BRANCH')
+        assertTestCallstackContains('githubscm.pushObject', 'origin, nightly-BRANCH, AUTHOR_CREDS_ID')
     }
 
     @Test
     void deploy_failing() throws Exception {
         helper.registerAllowedMethod('build', [Map.class], { map ->
-            if (map.get('job') == 'drools-deploy' || map.get('job') == 'kogito-runtimes-deploy') {
+            if (map.get('job') == 'drools.build-and-deploy' || map.get('job') == 'kogito-runtimes.build-and-deploy') {
                 return [
                     result: 'FAILURE',
                     absoluteUrl: 'URL',
@@ -90,14 +103,18 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
             'drools' : [:],
             'kogito-runtimes' : [:],
             'kogito-apps' : [:],
-            'kogito-examples': [ UPDATE_NIGHTLY_BRANCH: true ],
+            'kogito-examples': [:],
         ])
+
+        assertTestCallstackContains('githubscm.resolveRepository', 'kogito-examples, AUTHOR, BRANCH, false, AUTHOR_CREDS_ID')
+        assertTestCallstackContains('githubscm.createBranch', 'nightly-BRANCH')
+        assertTestCallstackContains('githubscm.pushObject', 'origin, nightly-BRANCH, AUTHOR_CREDS_ID')
     }
 
     @Test
     void deploy_unstable() throws Exception {
         helper.registerAllowedMethod('build', [Map.class], { map ->
-            if (map.get('job') == 'drools.deploy' || map.get('job') == 'kogito-apps-deploy') {
+            if (map.get('job') == 'drools.build-and-deploy' || map.get('job') == 'kogito-apps.build-and-deploy') {
                 return [
                     result: 'UNSTABLE',
                     absoluteUrl: 'URL',
@@ -115,8 +132,12 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
             'drools' : [:],
             'kogito-runtimes' : [:],
             'kogito-apps' : [:],
-            'kogito-examples': [ UPDATE_NIGHTLY_BRANCH: true ],
+            'kogito-examples': [:],
         ])
+
+        assertTestCallstackContains('githubscm.resolveRepository', 'kogito-examples, AUTHOR, BRANCH, false, AUTHOR_CREDS_ID')
+        assertTestCallstackContains('githubscm.createBranch', 'nightly-BRANCH')
+        assertTestCallstackContains('githubscm.pushObject', 'origin, nightly-BRANCH, AUTHOR_CREDS_ID')
     }
 
     @Test
@@ -126,13 +147,18 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
         thrown.expect(Exception)
 
         runJenkinsfileAndAssertFailure()
+
+        assertTestCallstackDoesNotContain('githubscm.resolveRepository')
+        assertTestCallstackDoesNotContain('githubscm.createBranch')
+        assertTestCallstackDoesNotContain('githubscm.pushObject')
     }
 
     void assertDeployBuildCalls(Map projects, boolean skipTests = false) {
-        assertBuildCalls(projects, 'deploy', [
+        assertBuildCalls(projects, '.build-and-deploy', [
                 DISPLAY_NAME: 'BRANCH-date',
                 SEND_NOTIFICATION: true,
                 SKIP_TESTS: skipTests,
+                SKIP_INTEGRATION_TESTS: skipTests,
             ])
     }
 
@@ -143,7 +169,7 @@ class TestJenkinsfileNightly extends SingleFileDeclarativePipelineTest {
             if (extraParams) {
                 buildParams.putAll(extraParams)
             }
-            assertBuildCall("${projectName}-${jobSuffix}", buildParams, true, false)
+            assertBuildCall("${projectName}${jobSuffix}", buildParams, true, false)
         }
     }
 
