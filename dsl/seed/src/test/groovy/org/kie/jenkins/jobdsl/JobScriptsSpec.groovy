847f4df3eb98357e359a05f38688f00627647ef2
Tristan Radisson <tristan.radisson@gmail.com>
2023-01-11 12:05:32 +0100

    KOGITO-8439 DSL: review main branch config (#766)

--- dsl/seed/src/test/groovy/org/kie/jenkins/jobdsl/JobScriptsSpec.groovy
+++ dsl/seed/src/test/groovy/org/kie/jenkins/jobdsl/JobScriptsSpec.groovy
@@ -49,7 +49,7 @@ class JobScriptsSpec extends Specification {
         envVars.put('GIT_BRANCH', 'GIT_BRANCH')
         envVars.put('GIT_AUTHOR_NAME', 'GIT_AUTHOR')
         envVars.put('GIT_AUTHOR', 'GIT_AUTHOR')
-        envVars.put('MAIN_BRANCHES', '{"default":"main"}')
+        envVars.put('MAIN_BRANCH', 'MAIN_BRANCH')
 
         envVars.put('SEED_AUTHOR', 'SEED_AUTHOR')
         envVars.put('SEED_BRANCH', 'SEED_BRANCH')
