17c695b11b2e1cb6dd4e1fbe88e7bc7b11eb705c
radtriste <tradisso@redhat.com>
2023-02-23 10:07:48 +0100

    DSL: Remove branch job: Add choice param

--- dsl/seed/jobs/root_jobs.groovy
+++ dsl/seed/jobs/root_jobs.groovy
@@ -61,7 +61,7 @@ def jobParamsRemove = [
 
 KogitoJobTemplate.createPipelineJob(this, jobParamsRemove)?.with {
     parameters {
-        stringParam('BRANCH_TO_REMOVE', '' , 'Branch to remove')
+        choice(name: 'BRANCH_TO_REMOVE', choices: "${ALL_BRANCHES}".split(',').retainAll { it != 'main' }, description: 'Which release branch to remove ?') 
     }
 
     environmentVariables {
