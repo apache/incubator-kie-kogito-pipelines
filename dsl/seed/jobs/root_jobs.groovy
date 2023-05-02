a555890a1fb2b214f0c56ccb4a13ca94f1366e65
Andrea Lamparelli <a.lamparelli95@gmail.com>
2023-05-02 16:11:41 +0200

    Added dry run mode for testing purposes

--- dsl/seed/jobs/root_jobs.groovy
+++ dsl/seed/jobs/root_jobs.groovy
@@ -106,10 +106,13 @@ if (communityReleaseBranches) {
         parameters {
             PRODUCTIZED_PROJECTS.split(',').each { projectName ->
                 choiceParam("${projectName}_RELEASE_BRANCH".toUpperCase(), communityReleaseBranches, "${Utils.getRepoNameCamelCase(projectName)} community branch to which to create the productized branch from")
+                stringParam("${projectName}_PROD_BRANCH_SUFFIX".toUpperCase(), 'prod', "${Utils.getRepoNameCamelCase(projectName)} productized branch suffix")
             }
 
             stringParam('QUARKUS_VERSION', '', 'Quarkus version to which to update all productized branches, usually latest LTS version')
             stringParam('ADDITIONAL_BUILD_MAVEN_OPTS', '', 'Additional default maven opts for jenkins jobs, e.g., -Ddata-index-ephemeral.image=quay.io/kiegroup/kogito-data-index-ephemeral')
+
+            booleanParam('DRY_RUN', false, 'If enabled no changes will be applied to remote branches')
         }
 
         environmentVariables {
