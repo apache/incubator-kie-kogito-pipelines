fcffaf1d7a78a1e0efb98864e42e539047fababb
radtriste <tradisso@redhat.com>
2023-08-17 14:12:40 +0200

    update seed jenkinsfile path

--- dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/KogitoConstants.groovy
+++ dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/KogitoConstants.groovy
@@ -4,7 +4,7 @@ class KogitoConstants {
 
     static String KOGITO_DEFAULT_PR_TRIGGER_PHRASE = '.*[j|J]enkins,?.*(retest|test) this.*'
 
-    static String SEED_JENKINSFILES_PATH = '.ci/jenkins'
+    static String SEED_JENKINSFILES_PATH = 'dsl/seed/jenkinsfiles'
 
     static String LABEL_DSL_TEST = 'dsl-test'
 
