07e7743700905ab8d14091a586493715787ed355
jstastny-cz <jan.stastny@ibm.com>
2023-10-02 13:51:05 +0200

    kie-issues#593: switch to compile downstream in Jenkins

--- dsl/scripts/pr_check.groovy
+++ dsl/scripts/pr_check.groovy
@@ -97,7 +97,7 @@ String getBuildChainCommandline() {
     String buildChainConfigRepo = env.BUILDCHAIN_CONFIG_REPO ?: 'incubator-kie-kogito-pipelines'
     String buildChainConfigBranch = env.BUILDCHAIN_CONFIG_BRANCH ?: '\${BRANCH:main}'
     String buildChainConfigGitAuthor = env.BUILDCHAIN_CONFIG_AUTHOR ?: '\${AUTHOR:apache}'
-    String buildChainConfigDefinitionFilePath = env.BUILDCHAIN_CONFIG_FILE_PATH ?: '.ci/pull-request-config.yaml'
+    String buildChainConfigDefinitionFilePath = env.BUILDCHAIN_CONFIG_FILE_PATH ?: '.ci/buildchain-config-pr-cdb.yaml'
 
     List buildChainAdditionalArguments = [
         "-p ${buildChainProject}",
