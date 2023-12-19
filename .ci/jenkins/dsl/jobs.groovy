a8bfd90bab888c35864333b107d7484fe54906a5
Tibor Zimányi <tiborzimanyi@ibm.com>
2023-12-19 14:00:49 +0100

    [kie-issues#784] Remove unneded environment CI configurations. (#1156)
    * [NO ISSUE] Remove unneeded environments from the CI builds.
    
    * [kie-issues#784] Remove unneeded environments from the CI builds.

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -59,17 +59,6 @@ setupNightlyJob()
 setupQuarkusPlatformJob(JobType.NIGHTLY)
 if (isMainStream()) {
     setupNightlyCloudJob()
-    setupQuarkus3NightlyJob()
-}
-
-KogitoJobUtils.createEnvironmentIntegrationBranchNightlyJob(this, 'quarkus-main')
-KogitoJobUtils.createEnvironmentIntegrationBranchNightlyJob(this, 'quarkus-lts')
-KogitoJobUtils.createEnvironmentIntegrationBranchNightlyJob(this, 'quarkus-branch')
-KogitoJobUtils.createEnvironmentIntegrationBranchNightlyJob(this, 'native-lts')
-KogitoJobUtils.createEnvironmentIntegrationBranchNightlyJob(this, 'quarkus-3', []) { script ->
-    def jobParams = JobParamsUtils.DEFAULT_PARAMS_GETTER(script)
-    jobParams.env.put('INTEGRATION_BRANCH_CURRENT', '2.x')
-    return jobParams
 }
 
 // Release
@@ -209,43 +198,6 @@ void setupNightlyCloudJob() {
 
             booleanParam('USE_TEMP_OPENSHIFT_REGISTRY', false, 'If enabled, use Openshift registry to push temporary images')
         }
-    }
-}
-
-void setupQuarkus3NightlyJob() {
-    // Tests are done on 9.x/2.x branch => Create 2.x branch on Kogito
-    // Need to split as Drools and Kogito end up in different integration branches
-    KogitoJobUtils.createNightlyBuildChainIntegrationJob(this, 'quarkus-3', 'drools', true) { script ->
-        def jobParams = JobParamsUtils.getDefaultJobParams(script, 'incubator-kie-drools')
-        jobParams.git.branch = VersionUtils.getProjectTargetBranch('drools', Utils.getGitBranch(this), Utils.getRepoName(this))
-        jobParams.env.put('ADDITIONAL_TIMEOUT', '180')
-        jobParams.env.put('BUILD_ENVIRONMENT_OPTIONS_CURRENT', 'rewrite push_changes')
-        jobParams.env.put('INTEGRATION_BRANCH_CURRENT', '9.x')
-        // jobParams.env.put('LAUNCH_DOWNSTREAM_JOBS', 'kie-jpmml-integration.integration,kogito-runtimes.integration')
-        jobParams.env.put('LAUNCH_DOWNSTREAM_JOBS', 'kogito-runtimes.integration')
-        jobParams.parametersValues.put('SKIP_TESTS', true)
-        jobParams.parametersValues.put('SKIP_INTEGRATION_TESTS', true)
-        return jobParams
-    }
-    // Commented as not migrated to Apache
-    // KogitoJobUtils.createBuildChainIntegrationJob(this, 'quarkus-3', 'kie-jpmml-integration', true) { script ->
-    //     def jobParams = JobParamsUtils.getDefaultJobParams(script, 'incubator-kie-jpmml-integration')
-    //     jobParams.env.put('ADDITIONAL_TIMEOUT', '180')
-    //     jobParams.env.put('BUILD_ENVIRONMENT_OPTIONS_CURRENT', 'rewrite push_changes')
-    //     jobParams.env.put('INTEGRATION_BRANCH_CURRENT', '9.x')
-    //     jobParams.parametersValues.put('SKIP_TESTS', true)
-    //     jobParams.parametersValues.put('SKIP_INTEGRATION_TESTS', true)
-    //     return jobParams
-    // }
-    KogitoJobUtils.createBuildChainIntegrationJob(this, 'quarkus-3', 'kogito-runtimes', true) { script ->
-        def jobParams = JobParamsUtils.getDefaultJobParams(script, 'incubator-kie-kogito-runtimes')
-        jobParams.env.put('ADDITIONAL_TIMEOUT', '720')
-        jobParams.env.put('BUILD_ENVIRONMENT_OPTIONS_CURRENT', 'rewrite push_changes')
-        jobParams.env.put('INTEGRATION_BRANCH_CURRENT', '2.x')
-        jobParams.env.put('BUILDCHAIN_FULL_BRANCH_DOWNSTREAM_BUILD', 'true')
-        jobParams.parametersValues.put('SKIP_TESTS', true)
-        jobParams.parametersValues.put('SKIP_INTEGRATION_TESTS', true)
-        return jobParams
     }
 }
 
