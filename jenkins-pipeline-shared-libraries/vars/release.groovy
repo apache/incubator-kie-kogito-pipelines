c45de89f07852045e14be811dc364e03c58e8fc6
Rodrigo Antunes <contato@rodrigoantunes.com.br>
2024-07-15 16:30:33 -0300

    Adjust pipelines to use the gpg key provided by Apache to sign the artifacts

--- jenkins-pipeline-shared-libraries/vars/release.groovy
+++ jenkins-pipeline-shared-libraries/vars/release.groovy
@@ -13,10 +13,27 @@ def gpgImportKeyFromFileWithPassword(String gpgKeyCredentialsId, String gpgKeyPa
     }
 }
 
+def gpgImportKeyFromStringWithoutPassword(String gpgKeyCredentialsId) {
+    withCredentials([string(credentialsId: gpgKeyCredentialsId, variable: 'SIGNING_KEY')]) {
+        // copy the key to singkey.gpg file in *plain text* so we can import it
+        sh """
+            cat $SIGNING_KEY > $WORKSPACE/signkey.gpg
+            # Please do not remove list keys command. When gpg is run for the first time, it may initialize some internals.
+            gpg --list-keys
+            gpg --batch --pinentry-mode=loopback --import signkey.gpg
+            rm $WORKSPACE/signkey.gpg
+        """
+    }
+}
+
 def gpgSignFileDetachedSignatureWithPassword(String file, String signatureTarget, String gpgKeyPasswordCredentialsId) {
     withCredentials([string(credentialsId: gpgKeyPasswordCredentialsId, variable: 'SIGNING_KEY_PASSWORD')]) {
         sh "gpg --batch --sign --pinentry-mode=loopback --passphrase \"${SIGNING_KEY_PASSWORD}\" --output ${signatureTarget} --detach-sig ${file}"
     }
+}
+
+def gpgSignFileDetachedSignatureWithoutPassword(String file, String signatureTarget) {
+    sh "gpg --batch --sign --pinentry-mode=loopback --output ${signatureTarget} --detach-sig ${file}"
 }
 
 boolean gpgIsValidDetachedSignature(String file, String signature) {
