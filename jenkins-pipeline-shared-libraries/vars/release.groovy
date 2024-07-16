b9d69e112a975981ee1d58689bba7e2a83761a77
Rodrigo Antunes <contato@rodrigoantunes.com.br>
2024-07-16 10:52:03 -0300

    Fix import gpg key function

--- jenkins-pipeline-shared-libraries/vars/release.groovy
+++ jenkins-pipeline-shared-libraries/vars/release.groovy
@@ -17,7 +17,7 @@ def gpgImportKeyFromStringWithoutPassword(String gpgKeyCredentialsId) {
     withCredentials([string(credentialsId: gpgKeyCredentialsId, variable: 'SIGNING_KEY')]) {
         // copy the key to singkey.gpg file in *plain text* so we can import it
         sh """
-            cat $SIGNING_KEY > $WORKSPACE/signkey.gpg
+            echo "$SIGNING_KEY" > $WORKSPACE/signkey.gpg
             # Please do not remove list keys command. When gpg is run for the first time, it may initialize some internals.
             gpg --list-keys
             gpg --batch --pinentry-mode=loopback --import signkey.gpg
